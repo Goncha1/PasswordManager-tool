28e2e2f67f5f9cbf0c773301ce146a70c140db6b
Gonchaa <153895388+Goncha1@users.noreply.github.com>
2024-09-10 00:41:02 +0400

    Update run_PasswordManager.sh

--- run_PasswordManager.sh
+++ run_PasswordManager.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+clear
+if command -v figlet > /dev/null; then
+    clear
+    figlet "Password Manager"
+else
+    echo "==== Password Manager ===="
+fi
 # Check if pycryptodome library is installed
 if python -c "import Crypto" &> /dev/null; then
     echo "pycryptodome library is installed."
